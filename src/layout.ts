c46741bac04f1cbfc0809573db89e98911f41fca
David J. Malan <malan@harvard.edu>
2022-07-14 15:10:58 -0400

    Checks for first .json file with workspace key

--- src/layout.ts
+++ src/layout.ts
@@ -16,123 +16,127 @@ const settingsPaths = [
 ];
 
 export async function prepareLayout(memento: vscode.Memento) {
-  const workspaceFolder = vscode.workspace.workspaceFolders![0];
-
-  let settingsUri: vscode.Uri | undefined;
-  for (let settingsPath of settingsPaths) {
-    const uri = vscode.Uri.joinPath(workspaceFolder.uri, settingsPath);
-
-    try {
-      await vscode.workspace.fs.stat(uri);
-      settingsUri = uri;
-      break;
-    } catch { }
-  }
-
-  if (!settingsUri) {
+  let settings = await getSettings();
+  if (!settings) {
     return;
   }
 
-  const fileBytes = await vscode.workspace.fs.readFile(settingsUri);
-  const fileContents = new TextDecoder().decode(fileBytes);
-
-  if (!fileContents) {
-    return;
+  if (memento) {
+    await vscode.commands.executeCommand(
+      "setContext",
+      HAS_LAYOUT_CONTEXT_KEY,
+      true
+    );
   }
 
-  try {
-    const settings = parse(fileContents);
-    if (!settings.workspace) {
-      return;
+  const isCodespaceActivation = memento && IS_CODESPACE;
+
+  // Defaults
+  if (settings === true) {
+    settings = {
+      "view": "readme",
+      "files": [],
+      "terminals": [""]
+    };
+  }
+  else {
+    if (!("view" in settings)) {
+      settings.view = "readme";
+    }
+    if (!("files" in settings)) {
+      settings.files = [];
+    }
+    if (!("terminals" in settings)) {
+      settings.terminals = [""];
+    }
+    if (!("browser" in settings)) {
+      settings.browser = null;
+    }
+  }
+
+  setTimeout(async () => {
+    if (settings.view) {
+      try {
+        let view = settings.view;
+        if (view === "readme") {
+          view = "workspace-layout.readme";
+          await vscode.commands.executeCommand(
+            "setContext",
+            HAS_README_CONTEXT_KEY,
+            true
+          );
+        }
+        if (view) {
+          vscode.commands.executeCommand(`${view}.focus`);
+        }
+      } catch {
+        console.error("The configured view wasn't found: ", settings.view);
+      }
     }
 
-    if (memento) {
-      await vscode.commands.executeCommand(
-        "setContext",
-        HAS_LAYOUT_CONTEXT_KEY,
-        true
-      );
-    }
+    if (settings.terminals) {
+      if (!vscode.workspace.isTrusted) {
+        // Wait for the user to grant trust to the workspace,
+        // before attempting to launch any configured terminals.
+        const trustHandler = vscode.workspace.onDidGrantWorkspaceTrust(() => {
+          createTerminals(settings.terminals);
+          trustHandler.dispose();
+        });
+        return;
+      }
+      if (isCodespaceActivation) {
+        child_process.execSync("touch $HOME/.config/vscode-dev-containers/first-run-notice-already-displayed");
+      }
+      createTerminals(settings.terminals);
 
-    const isCodespaceActivation = memento && IS_CODESPACE;
-    let workspaceConfig = settings.workspace;
-
-    // Defaults
-    if (workspaceConfig === true) {
-      workspaceConfig = {
-        "view": "readme",
-        "files": [],
-        "terminals": [""]
-      };
-    }
-    else {
-      if (!("view" in workspaceConfig)) {
-        workspaceConfig.view = "readme";
-      }
-      if (!("files" in workspaceConfig)) {
-        workspaceConfig.files = [];
-      }
-      if (!("terminals" in workspaceConfig)) {
-        workspaceConfig.terminals = [""];
-      }
-      if (!("browser" in workspaceConfig)) {
-        workspaceConfig.browser = null;
-      }
-    }
-
-    setTimeout(async () => {
-      if (workspaceConfig.view) {
-        try {
-          let view = workspaceConfig.view;
-          if (view === "readme") {
-            view = "workspace-layout.readme";
-            await vscode.commands.executeCommand(
-              "setContext",
-              HAS_README_CONTEXT_KEY,
-              true
-            );
-          }
-          if (view) {
-            vscode.commands.executeCommand(`${view}.focus`);
-          }
-        } catch {
-          console.error("The configured view wasn't found: ", workspaceConfig.view);
-        }
+      if (settings.files) {
+        await createFiles(settings.files);
       }
 
-      if (workspaceConfig.terminals) {
-        if (!vscode.workspace.isTrusted) {
-          // Wait for the user to grant trust to the workspace,
-          // before attempting to launch any configured terminals.
-          const trustHandler = vscode.workspace.onDidGrantWorkspaceTrust(() => {
-            createTerminals(workspaceConfig.terminals);
-            trustHandler.dispose();
-          });
-          return;
-        }
-        if (isCodespaceActivation) {
-          child_process.execSync("touch $HOME/.config/vscode-dev-containers/first-run-notice-already-displayed");
-        }
-        createTerminals(workspaceConfig.terminals);
+      if (settings.browser) {
+        await vscode.commands.executeCommand("simpleBrowser.api.open",
+          vscode.Uri.parse(settings.browser), { viewColumn: vscode.ViewColumn.Beside });
+      }
 
-        if (workspaceConfig.files) {
-          await createFiles(workspaceConfig.files);
-        }
-
-        if (workspaceConfig.browser) {
-          await vscode.commands.executeCommand("simpleBrowser.api.open",
-            vscode.Uri.parse(workspaceConfig.browser), { viewColumn: vscode.ViewColumn.Beside });
-        }
-
-        // Activate first editor in each group, from right to left
-        for (let i = vscode.window.tabGroups.all.length - 1; i >= 0; i--) {
-          const tabGroup = vscode.window.tabGroups.all[i];
+      // Activate first editor in each group, from right to left
+      for (let i = vscode.window.tabGroups.all.length - 1; i >= 0; i--) {
+        const tabGroup = vscode.window.tabGroups.all[i];
+        if (tabGroup.tabs.length > 0) {
           const input = tabGroup.tabs[0].input as vscode.TabInputText;
           vscode.window.showTextDocument(input.uri, { viewColumn: i + 1 });
         }
       }
-    }, isCodespaceActivation ? 5000 : 0);
-  } catch {
-    console.error("Workspace layout configuration appears to be invalid JSON.");
+    }
+  }, isCodespaceActivation ? 5000 : 0);
+}
+
+async function getSettings() {
+  const workspaceFolder = vscode.workspace.workspaceFolders![0];
+  for (let settingsPath of settingsPaths) {
+    const uri = vscode.Uri.joinPath(workspaceFolder.uri, settingsPath);
+    let fileContents;
+    try {
+      const fileBytes = await vscode.workspace.fs.readFile(uri);
+      fileContents = new TextDecoder().decode(fileBytes);
+    }
+    catch {
+      continue;
+    }
+    let settings;
+    try {
+      settings = parse(fileContents);
+    }
+    catch {
+      console.error("Invalid JSON in " + settingsPath);
+      continue;
+    }
+    if ("workspace" in settings) {
+      const workspace = settings.workspace;
+      if (workspace === true || typeof workspace === "object") {
+        return workspace;
+      }
+      console.error("Invalid value for workspace in " + settingsPath);
+    }
   }
+  return null;
 }
